72e240cd0ceed85facb168d13f3717ca4e487716
Dara Lenaghan <g00385153@gmit.ie>
2023-03-30 12:23:07 +0100

    first commit

--- data.js
+++ data.js
@@ -2,42 +2,42 @@
 const data = {
     products: [
         {
-            name: 'iPhone 14',
-            slug: 'iphone14',
-            category: 'Apple',
-            image: '/Images/iPhone14.jpg', //653 x 914
+            name: "iPhone 14",
+            slug: "iphone14",
+            category: "Apple",
+            image: "/Images/iPhone14.jpg", //653 x 914
             price: 400,
             stockCount: 10,
-            brand: 'Apple',
+            brand: "Apple",
             rating: 4.5,
             numReviews: 10,
             description: "Apple's iPhone 14 models are identical in design to the iPhone 13 models, featuring flat edges, an aerospace-grade aluminum enclosure, and a glass back that enables wireless charging. The iPhone 14 models come in blue, purple, midnight, starlight",
 
         },
         {
-            name: 'Samsung Galaxy S22',
-            slug: 'GalaxyS22',
+            name: "Samsung Galaxy S22",
+            slug: "GalaxyS22",
             category: 'Samsung',
-            image: '/Images/s22.jpg',
+            image: "/Images/s22.jpg",
             price: 400,
             stockCount: 10,
-            brand: 'Apple',
+            brand: "Apple",
             rating: 4.5,
             numReviews: 10,
-            description: '',
+            description: "",
 
         },
         {
-            name: 'One Plus 10',
-            slug: 'oneplus10',
+            name: "One Plus 10",
+            slug: "oneplus10",
             category: 'One Plus',
-            image: '/Images/oneplus10.jpg',
+            image: "/Images/oneplus10.jpg",
             price: 400,
             stockCount: 10,
-            brand: 'Apple',
+            brand: "Apple",
             rating: 4.5,
             numReviews: 10,
-            description: '',
+            description: "",
 
         },
     ],
